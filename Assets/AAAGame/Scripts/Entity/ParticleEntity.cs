e937e901e08beaff4575b5e44f98ac28fd5bfe2c
LeaderOne <Z1015558261@outlook.com>
2024-10-25 22:10:38 +0800

    Initial commit

---
+++ Assets/AAAGame/Scripts/Entity/ParticleEntity.cs
@@ -0,0 +1,42 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+public class ParticleEntity : EntityBase
+{
+    public const string LIFE_TIME = "LifeTime";
+    public const string SORT_LAYER = "SortLayer";
+    bool autoHide;
+    float lifeTime;
+    protected override void OnShow(object userData)
+    {
+        base.OnShow(userData);
+        autoHide = true;
+
+        lifeTime = Params.Get<VarFloat>(LIFE_TIME, 2f);
+
+        autoHide = lifeTime > 0;
+
+        if (Params.TryGet<VarInt32>(SORT_LAYER, out var pSortLayer))
+        {
+            SetParticlesSortLayer(pSortLayer);
+        }
+
+        if (autoHide)
+        {
+            UniTask.Delay((int)(lifeTime * 1000)).ContinueWith(() =>
+            {
+                GF.Entity.HideEntitySafe(this);
+            }).Forget();
+        }
+    }
+    private void SetParticlesSortLayer(int layer)
+    {
+        var particles = GetComponentsInChildren<ParticleSystem>(true);
+        foreach (ParticleSystem item in particles)
+        {
+            var render = item.GetComponent<Renderer>();
+            render.sortingOrder = layer;
+        }
+    }
+}
